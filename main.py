0510cd6133d8f22c873ac3ae38309abe13256a45
Talon <talonpollard@gmail.com>
2020-01-22 16:17:35 -0800

    Update: Check calendar events on command

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ from google.auth.transport.requests import Request
 
 import os
 import time
-import playsound
+import pyttsx3
 import speech_recognition as sr
-from gtts import gTTS
+import pytz
 
 import wmi # control brightness
 import json
@@ -24,10 +24,9 @@ DAY_EXTENTIONS = ["rd", "th", "st", "nd"]
 
 
 def speak(text):
-    tts = gTTS(text=text, lang="en")
-    fname = "voice.mp3"
-    tts.save(fname)
-    playsound.playsound(fname)
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
 
 def get_audio():
     print("Ready to listen...")
@@ -70,6 +69,9 @@ def run(here):
 #     target = text.split(" ")
 #     speak(f"Hello, Talon's {target[-1]}")
 
+# if "go to sleep" in text:
+#     os.system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0")
+
 
 # if __name__ == "__main__":
 #     with open("key.json") as f:
@@ -99,20 +101,35 @@ def authenticate_google():
 
     return service
 
-def get_events(n, service):
+def get_events(day, service):
     # Call the Calendar API
-    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    print(f'Getting the upcoming {n} events')
-    events_result = service.events().list(calendarId='primary', timeMin=now,
-                                        maxResults=n, singleEvents=True,
+    date = datetime.datetime.combine(day, datetime.datetime.min.time())
+    end_date = datetime.datetime.combine(day, datetime.datetime.max.time())
+    utc = pytz.UTC
+    date = date.astimezone()
+    end_date = end_date.astimezone(utc)
+
+    events_result = service.events().list(calendarId='primary', timeMin=date.isoformat(), timeMax=end_date.isoformat(),
+                                        singleEvents=True,
                                         orderBy='startTime').execute()
     events = events_result.get('items', [])
 
     if not events:
-        print('No upcoming events found.')
-    for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
+        speak('No upcoming events found.')
+    else:
+        speak(f"You have {len(events)} events on this day.")
+        for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            print(start, event['summary'])
+
+            start_time = str(start.split("T")[1].split("-")[0])
+            if int(start_time.split(":")[0]) < 12:
+                start_time = start_time + "AM"
+            else:
+                start_time = str(int(start_time.split(":")[0]) - 12) + str(start_time.split(":")[1])
+                start_time = start_time + "PM"
+
+            speak(event["summary"] + " at " + start_time)
 
 
 def get_date(text):
@@ -160,6 +177,9 @@ def get_date(text):
         
         return today + datetime.timedelta(dif)
 
+    if month == -1 or day == -1:
+        return None
+
     return datetime.date(month=month, day=day, year=year)
 
 # service = authenticate_google()
@@ -167,5 +187,21 @@ def get_date(text):
 
 # if __name__ == '__main__':
 
-text = get_audio().lower()
-print(F"This nigga really just said {get_date(text)}, LMFAOOOO") 
+
+
+# text = get_audio().lower()
+# print(F"This nigga really just said {get_date(text)}, LMFAOOOO") 
+
+service = authenticate_google()
+text = get_audio()
+
+CALENDAR_STRS = ["today", "plan", "planned", "plans",
+                    "am i busy", "what do i have"]
+for phrase in CALENDAR_STRS:
+    if phrase in text.lower():
+        date = get_date(text)
+        if date:
+            get_events(get_date(text), service)
+        else:
+            speak("Please try again.")
+        break
