23a4f16a92fa8d4c6776ee5bd8b4e4da2a3b0aeb
Talon Pollard <Talon.Pollard@vvc.edu>
2020-02-24 16:24:53 -0800

    Update file

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ Serena voice assistant is currently in development. Serena can or will be able t
 - Recall events from Google Calendar
 - Attempt Google search 
 - Play music (comes from Youtube music)
+
+
+# References
+- https://towardsdatascience.com/build-your-first-voice-assistant-85a5a49f6cc1
--- main.py
+++ main.py
@@ -1,23 +1,47 @@
 # Google Calendar
-from __future__ import print_function
-import datetime
-import pickle
-import os.path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
+# from __future__ import print_function
+# import datetime
+# import pickle
+# import os.path
+# from googleapiclient.discovery import build
+# from google_auth_oauthlib.flow import InstalledAppFlow
+# from google.auth.transport.requests import Request
 
+# import os
+# import sys
+# import time
+# import pyttsx3
+# import speech_recognition as sr
+# import pytz
+# import subprocess
+
+# import json
+
+# import system
+
+
+#####
+import speech_recognition as sr
 import os
 import sys
-import time
-import pyttsx3
-import speech_recognition as sr
-import pytz
+import re
+import webbrowser
+import smtplib
+import requests
 import subprocess
-
+from pyowm import OWM
+import youtube_dl
+import vlc
+import urllib
+import urllib2
 import json
+from bs4 import BeautifulSoup as soup
+from urllib2 import urlopen
+import wikipedia
+import random
+from time import strftime
 
-import system
+
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
@@ -25,6 +49,7 @@ MONTHS = ["january", "february", "march", "april", "may", "june","july", "august
 DAYS = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
 DAY_EXTENTIONS = ["rd", "th", "st", "nd"]
 
+# Speak back text to user
 def speak(text):
     engine = pyttsx3.init()
     engine.setProperty('voice', "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0")
@@ -32,6 +57,7 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
+# Listen to user
 def get_audio():
     # print("Ready to listen...")
     r = sr.Recognizer()
@@ -49,6 +75,27 @@ def get_audio():
     print("Ready to listen...")
             
     return said.lower()
+
+def listen():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print('Say something...')
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source, duration=1)
+        audio = r.listen(source)
+    try:
+        command = r.recognize_google(audio).lower()
+        print('You said: ' + command + '\n')
+    #loop back to continue to listen for commands if unrecognizable speech is received
+    except sr.UnknownValueError:
+        print('....')
+        command = myCommand();
+    return command
+
+def respond(audio):
+    print(audio)
+    for line in audio.splitlines():
+        os.system("say " + audio)
 
 
 def authenticate_google():
@@ -189,6 +236,24 @@ def volume(text):
         else:
             system.Audio().setVolume(pre="to",level=level)
 
+# while True:
+#     assistant(myCommand())
+
+#     if 'open' in command:
+#         reg_ex = re.search('open (.+)', command)
+#         if reg_ex:
+#             domain = reg_ex.group(1)
+#             print(domain)
+#             if "twitter" in command:
+#                 domain = "twitter.com"
+#             elif "facebook" in command:
+#                 domain = "fb.com"
+#             url = 'https://www.' + domain
+#             webbrowser.open(url)
+#             respond('Website has been opened.')
+#         else:
+#             pass
+
 
 '''
 WAKE = "serena"
@@ -247,4 +312,19 @@ while listen:
 '''
 
 
-# if __name__ == '__main__':
+if __name__ == '__main__':
+    command = "open facebook"
+    if 'open' in command:
+        reg_ex = re.search('open (.+)', command)
+        if reg_ex:
+            domain = reg_ex.group(1)
+            print(domain)
+            if "twitter" in command:
+                domain = "twitter.com"
+            elif "facebook" in command:
+                domain = "fb.com"
+            url = 'https://www.' + domain
+            webbrowser.open(url)
+            respond('Website has been opened.')
+        else:
+            pass
